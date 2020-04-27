bed2fe343146aeb40345182124274edd82e5ee97
Ormonali <46989835+Ormonali@users.noreply.github.com>
2020-04-27 15:29:35 +0600

    Update LowercaseRoutes.php

--- app/Http/Middleware/LowercaseRoutes.php
+++ app/Http/Middleware/LowercaseRoutes.php
@@ -17,7 +17,7 @@ class LowercaseRoutes
     public function handle($request, Closure $next)
     {
         $word = explode("/",urldecode($request->path()));
-        if(preg_match('/[А-Яа-яЁё]/u', urldecode($request->path())) and preg_replace('/[А-ЯЁ]+/u', '', urldecode($request->path()))!=urldecode($request->path()) ){
+        if(preg_match('/[А-Яа-яЁё]/u', urldecode($request->path())) or preg_replace('/[А-ЯЁ]+/u', '', urldecode($request->path()))!=urldecode($request->path()) ){
           $new_route = str_replace($request->path(), $word[0].'/'.str_replace(' ','-',ruToEnLetter($word[1])), $request->fullUrl());
           return Redirect::to($new_route, 301);
         }else if(!ctype_lower(preg_replace('/[^A-Za-z]/', '', $request->path()))){
