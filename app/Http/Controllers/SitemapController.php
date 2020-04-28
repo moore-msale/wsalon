07b4719f749c8e0a7afc5691eef75e8324d78deb
Ormonali <46989835+Ormonali@users.noreply.github.com>
2020-04-28 14:22:30 +0600

    Update SitemapController.php

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Tags\Url;
 
 class SitemapController extends Controller
 {
     public function updateSitemap(){
-        SitemapGenerator::create('https://wsalon-showroom.com')->writeToFile('sitemap.xml');
-        return redirect('/moo');
+
+        SitemapGenerator::create('https://wsalon-showroom.com')->hasCrawled(function (Url $url) {
+        if($url->url == 'https://wsalon-showroom.com/'){
+          return;
+        }
+        return $url;
+        })->writeToFile('sitemap.xml');
+
+        return redirect('/sitemap.xml');
     }
 }
